207e4551e4019d4f1e8ad34ee123a8e048264c1f
kimj39 <ji.woo.kim@vanderbilt.edu>
2017-10-21 17:29:52 -0500

    asd

--- vh4/script/navbar.js
+++ vh4/script/navbar.js
@@ -9,6 +9,6 @@ $(".navbar").on('click', function(e){
   var myClass = e.target.className;
   if (myClass === "navOp" || myClass === "navText") {
     var myId = e.target.id;  
-    $("#content").load("vh4/views/" + myId + ".html #" + myId + "Page");
+    $("#content").load("vh4/view/" + myId + ".html #" + myId + "Page");
   }
 }); 
